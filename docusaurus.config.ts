fcceceb7e8148e44cbfc231b5ffb3f8435588a40
Kent Bull <kent@kentbull.com>
2024-08-27 12:08:58 -0600

    refactor: use custom url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://drand.github.io',
+  url: 'https://docs.drand.love',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
