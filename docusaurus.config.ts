ab2551b1f78ff69230722d9603053d942b89ed24
Kent Bull <kent@kentbull.com>
2024-08-27 12:03:08 -0600

    refactor: remove /drand-docs/ path suffix
    This is to support hosting the docs at https://docs.drand.love

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://drand.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/drand-docs',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
