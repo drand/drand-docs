fb696845900cc3a4377ce1777293de6217b8d932
Justin Hull <dev@justinchull.com>
2024-09-09 23:43:40 -0600

    Modified th broken links configuration.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   organizationName: 'drand', // Usually your GitHub org/user name.
   projectName: 'drand-docs', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
